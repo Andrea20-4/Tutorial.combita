2fe2f043dfbf8384c497035aba712a3db948f928
Andrea20-4 <aaguirrepaula@gmail.com>
2025-03-19 15:58:58 -0500

    taller listo para presentacion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Seleccionamos el párrafo y el botón
+
 const ejemploTexto = document.getElementById('ejemplo1');
 const btnChangeColor = document.getElementById('btnChangeColor');
 
-// Agregamos un event listener al botón
+
 btnChangeColor.addEventListener('click', () => {
   // Si el texto está en azul, lo cambiamos a rojo; si está en rojo, lo volvemos azul
   if (ejemploTexto.style.color === 'blue') {
